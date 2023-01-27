befa515e709c5f8e4018f9efc6ed0be445fe6277
adamjohnlea <imnogeekdptcom@me.com>
2023-01-27 16:52:23 -0700

    modify explore behavior for logged in user

--- app/Http/Controllers/ExploreController.php
+++ app/Http/Controllers/ExploreController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class ExploreController extends Controller
 {
 
-    public function index(Request $request) {
+    public function index() {
         return view('explore-index', [
             'users' => User::all(),
             'top5'  => User::withCount('posts')->orderBy('posts_count', 'desc')->take(5)->get()
--- resources/views/explore-index.blade.php
+++ resources/views/explore-index.blade.php
@@ -8,10 +8,17 @@
     <div class="list-group">
         @foreach($top5 as $user)
             <div class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
-                <a href="/profile/{{ $user->username }}" class="">
-                    <img class="avatar-tiny" src="{{ $user->avatar }}" />
-                    <strong>{{ $user->username }}</strong> has made {{ $user->posts_count }} {{ Str::plural( 'post', $user->posts_count ) }}
-                </a>
+                @if(auth()->user()->id !== $user->id)
+                    <a href="/profile/{{ $user->username }}" class="">
+                        <img class="avatar-tiny" src="{{ $user->avatar }}" />
+                        <strong>{{ $user->username }}</strong> has made {{ $user->posts_count }} {{ Str::plural( 'post', $user->posts_count ) }}
+                    </a>
+                @else
+                    <span>
+                        <img class="avatar-tiny" src="{{ $user->avatar }}" />
+                        <strong>{{ $user->username }}</strong> You've made {{ $user->posts_count }} {{ Str::plural( 'post', $user->posts_count ) }}
+                    </span>
+                @endif
                 @if(auth()->user()->id !== $user->id)
                     @if(!auth()->user()->isFollowing($user))
                     <form class="ml-auto d-inline" action="/create-follow/{{ $user->username }}" method="POST">
@@ -30,15 +37,19 @@
     </div>
 
     <h2 class="mt-4">Find Friends</h2>
-
     <div class="list-group">
         @foreach($users as $user)
-            <a href="/profile/{{ $user->username }}" class="list-group-item list-group-item-action">
-                <img class="avatar-tiny" src="{{ $user->avatar }}" />
-                <strong>{{ $user->username }}</strong>
-            </a>
+            @if(auth()->user()->id !== $user->id)
+                <div class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                    <a href="/profile/{{ $user->username }}" class="">
+                        <img class="avatar-tiny" src="{{ $user->avatar }}" />
+                        <strong>{{ $user->username }}</strong>
+                    </a>
+                </div>
+            @endif
         @endforeach
     </div>
+
 </div>
 
 </x-layout>
