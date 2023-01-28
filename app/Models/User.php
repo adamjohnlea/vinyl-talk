6b6eaf3a4dae5824a035c3c33a4005244aeb446b
adamjohnlea <imnogeekdptcom@me.com>
2023-01-28 14:16:27 -0700

    finish user profile with followers and following relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,6 +53,14 @@ class User extends Authenticatable
         return $this->hasMany(Post::class, 'user_id');
     }
 
+    public function followers() {
+        return $this->hasMany(Follow::class, 'followeduser');
+    }
+
+    public function followingTheseUsers() {
+        return $this->hasMany(Follow::class, 'user_id');
+    }
+
     /**
      * Determines if the current user is following the given user.
      *
