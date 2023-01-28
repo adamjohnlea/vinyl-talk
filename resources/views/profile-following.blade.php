d79551a42251d8c661d2abe0883ca9ef50de0964
adamjohnlea <imnogeekdptcom@me.com>
2023-01-28 15:46:45 -0700

    add post component and template titles

--- resources/views/profile-following.blade.php
+++ resources/views/profile-following.blade.php
@@ -1,4 +1,4 @@
-<x-profile :sharedData="$sharedData">
+<x-profile :sharedData="$sharedData"  doctitle="{{ $sharedData['username'] }} is Following">
     <div class="list-group">
         @foreach($following as $follow)
             <a href="/profile/{{ $follow->userBeingFollowed->username }}" class="list-group-item list-group-item-action">
