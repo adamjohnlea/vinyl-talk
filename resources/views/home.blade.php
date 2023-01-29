8b0d0e73b4a1ecf31d8f7edd4d807a8d161437cd
adamjohnlea <imnogeekdptcom@me.com>
2023-01-28 23:17:30 -0700

    remove redundant sentence on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <div class="row align-items-center">
         <div class="col-lg-7 py-3 py-md-5">
             <h1 class="display-3">Love Vinyl?</h1>
-            <p class="lead text-muted">You are not alone. Vinyl records have seen a resurence in popularity in recent years. The warm rich sounds of vinyl are once again filling millions of homes around the world. Share your love vinyl with others, discuss releases both new and old. Shar your love of vinyls with others!</p>
+            <p class="lead text-muted">You are not alone. Vinyl records have seen a resurence in popularity in recent years. The warm rich sounds of vinyl are once again filling millions of homes around the world. Share your love vinyl with others, discuss releases both new and old. </p>
         </div>
         <div class="col-lg-5 pl-lg-5 pb-3 py-lg-5">
             <form action="/register" method="POST" id="registration-form">
