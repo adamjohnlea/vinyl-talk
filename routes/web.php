89ef8957da9d959da0b8c392f2543a9c8956246f
adamjohnlea <imnogeekdptcom@me.com>
2023-01-28 23:30:56 -0700

    cosmetic changes to routes file

--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,24 @@ use App\Http\Controllers\ExploreController;
 |
 */
 
-Route::get ('/',            [UserController::class, "showCorrectHomepage"])->name('login');
-Route::post('/register',    [UserController::class, "register"])->middleware('guest');
-Route::post('/login',       [UserController::class, "login"])->middleware('guest');
-Route::post('/logout',      [UserController::class, "logout"])->middleware('auth');
-Route::get('manage-avatar', [UserController::class, "showAvatarForm"])->middleware('auth');
-Route::post('manage-avatar', [UserController::class, "storeAvatar"])->middleware('auth');
+// User Related (non profile) Routes
+Route::get ('/',                [UserController::class, "showCorrectHomepage"])->name('login');
+Route::post('/register',        [UserController::class, "register"])->middleware('guest');
+Route::post('/login',           [UserController::class, "login"])->middleware('guest');
+Route::post('/logout',          [UserController::class, "logout"])->middleware('auth');
+Route::get('/manage-avatar',    [UserController::class, "showAvatarForm"])->middleware('auth');
+Route::post('/manage-avatar',   [UserController::class, "storeAvatar"])->middleware('auth');
 
-Route::get ( 'explore', [ ExploreController::class, "index" ] )->middleware('auth');
+// User Profile Related Routes
+Route::get ('/profile/{user:username}',             [UserController::class, 'profile']);
+Route::get ('/profile/{user:username}/followers',   [UserController::class, 'profileFollowers']);
+Route::get ('/profile/{user:username}/following',   [UserController::class, 'profileFollowing']);
 
-Route::post('create-follow/{user:username}', [FollowController::class, "createFollow"])->middleware('auth');
-Route::post('remove-follow/{user:username}', [FollowController::class, "removeFollow"])->middleware('auth');
+// Follow Related Routes
+Route::post('/create-follow/{user:username}', [FollowController::class, "createFollow"])->middleware('auth');
+Route::post('/remove-follow/{user:username}', [FollowController::class, "removeFollow"])->middleware('auth');
 
+// Post Related Routes
 Route::get ('/create-post',         [PostController::class, "showCreateForm"])->middleware('auth');
 Route::post('/create-post',         [PostController::class, "storeNewPost"])->middleware('auth');
 Route::get ('/post/{post}',         [PostController::class, "showSinglePost"]);
@@ -37,10 +43,9 @@ Route::get ('/post/{post}/edit',    [PostController::class, "showEditForm"])->mi
 Route::put ('/post/{post}',         [PostController::class, "actuallyUpdate"])->middleware('can:update,post');
 Route::get('/search/{term}',        [PostController::class, "search"]);
 
-Route::get ('/profile/{user:username}', [UserController::class, 'profile']);
-Route::get ('/profile/{user:username}/followers', [UserController::class, 'profileFollowers']);
-Route::get ('/profile/{user:username}/following', [UserController::class, 'profileFollowing']);
+// Explore Related Routes
+Route::get ( '/explore',        [ ExploreController::class, "index" ] )->middleware('auth');
 
-Route::get ('admins-only', function() {
-    return "only admins!";
-})->middleware('can:visitAdminPages');
+//Route::get ('/admins-only', function() {
+//    return "only admins!";
+//})->middleware('can:visitAdminPages');
